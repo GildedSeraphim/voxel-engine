26b617e7d21b7d92af8800a9d56d5e1e8d6f132d
GildedSeraphim <nathanishaaz@gmail.com>
2025-02-23 08:57:57 -0600

    rollback to learn

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,10 @@
 const std = @import("std");
 const c = @import("clibs.zig");
-const window = @import("render/window.zig");
 const math = @import("math.zig");
-const vk = @import("render/vulkan.zig");
-const render = @import("render/render.zig");
 
 pub const window_width: usize = 800;
 pub const window_height: usize = 600;
 
 pub fn main() !void {
-    const w = try window.Window.create(window_width, window_height, "Sample Window");
-    defer w.destroy();
-
-    const r = try render.initVulkan();
-    defer r.destroy();
-
-    while (!w.shouldClose()) {
-        c.glfwPollEvents();
-    }
+    std.debug.print("Hello World!\nDimensions are {} and {}", .{ window_width, window_height });
 }
